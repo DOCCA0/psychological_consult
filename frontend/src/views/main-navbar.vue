e275e90ef99347ed1dfc549e9dfb3f3f76f35a93
drp <1185245104@qq.com>
2021-06-24 19:27:41 +0800

    .

--- frontend/src/views/main-navbar.vue
+++ frontend/src/views/main-navbar.vue
@@ -2,8 +2,8 @@
   <nav class="site-navbar" :class="'site-navbar--' + navbarLayoutType">
     <div class="site-navbar__header">
       <h1 class="site-navbar__brand" @click="$router.push({ name: 'home' })">
-        <a class="site-navbar__brand-lg" href="javascript:;">人人快速开发平台</a>
-        <a class="site-navbar__brand-mini" href="javascript:;">人人</a>
+        <a class="site-navbar__brand-lg" href="javascript:;">心理咨询平台</a>
+        <a class="site-navbar__brand-mini" href="javascript:;">心理</a>
       </h1>
     </div>
     <div class="site-navbar__body clearfix">
