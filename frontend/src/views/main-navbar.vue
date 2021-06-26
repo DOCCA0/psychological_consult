a230326c52ef9400e250eb1ec5ec262d080bc814
drp <1185245104@qq.com>
2021-06-26 11:47:33 +0800

    删除2

--- frontend/src/views/main-navbar.vue
+++ frontend/src/views/main-navbar.vue
@@ -25,12 +25,6 @@
           </template>
         </el-menu-item>
 
-        <el-submenu index="3">
-          <template slot="title">Git源码</template>
-          <el-menu-item index="2-1"><a href="https://github.com/renrenio/renren-fast-vue" target="_blank">前端</a></el-menu-item>
-          <el-menu-item index="2-2"><a href="https://gitee.com/renrenio/renren-fast" target="_blank">后台</a></el-menu-item>
-          <el-menu-item index="2-3"><a href="https://gitee.com/renrenio/renren-generator" target="_blank">代码生成器</a></el-menu-item>
-        </el-submenu>
         <el-menu-item class="site-navbar__avatar" index="3">
           <el-dropdown :show-timeout="0" placement="bottom">
             <span class="el-dropdown-link">
