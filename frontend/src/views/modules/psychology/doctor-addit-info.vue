aa85cb08277aad8547915f32b1a5aef339d0502a
201017yushiYAO <972085744@qq.com>
2021-06-27 13:22:24 +0800

    applying completed

---
+++ frontend/src/views/modules/psychology/doctor-addit-info.vue
@@ -0,0 +1,245 @@
+<template>
+  <div class="mod-user">
+    <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
+      <el-form-item>
+        <el-input v-model="dataForm.userName" placeholder="用户名" clearable></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="getDataList()">查询</el-button>
+        <el-button v-if="isAuth('sys:user:save')" type="primary" @click="addOrUpdateHandle()">新增</el-button>
+        <el-button v-if="isAuth('sys:user:delete')" type="danger" @click="deleteHandle()" :disabled="dataListSelections.length <= 0">批量删除</el-button>
+      </el-form-item>
+    </el-form>
+    <!--    表格-->
+    <el-table
+      :data="dataList"
+      border
+      v-loading="dataListLoading"
+      @selection-change="selectionChangeHandle"
+      style="width: 100%;">
+      <el-table-column
+        type="selection"
+        header-align="center"
+        align="center"
+        width="50">
+      </el-table-column>
+      <el-table-column
+        prop="userId"
+        header-align="center"
+        align="center"
+        width="80"
+        label="ID">
+      </el-table-column>
+      <el-table-column
+        prop="doctorId"
+        header-align="center"
+        align="center"
+        label="医生编号">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        header-align="center"
+        align="center"
+        label="姓名">
+      </el-table-column>
+      <el-table-column
+        prop="sex"
+        header-align="center"
+        align="center"
+        label="性别">
+        <template slot-scope="scope">{{ scope.row.sex === 0 ? '男' : '女' }}</template>
+      </el-table-column>
+      <el-table-column
+        prop="age"
+        header-align="center"
+        align="center"
+        label="年龄">
+      </el-table-column>
+      <el-table-column
+        prop="level"
+        header-align="center"
+        align="center"
+        label="级别">
+      </el-table-column>
+      <el-table-column
+        prop="skill"
+        header-align="center"
+        align="center"
+        label="擅长领域">
+      </el-table-column>
+      <el-table-column
+        prop="place"
+        header-align="center"
+        align="center"
+        label="工作地点">
+      </el-table-column>
+      <el-table-column
+        prop="workTime"
+        header-align="center"
+        align="center"
+        label="工作时间">
+      </el-table-column>
+      <el-table-column
+        prop="status"
+        header-align="center"
+        align="center"
+        label="状态">
+        <template slot-scope="scope">
+          <el-tag v-if="scope.row.status === 0" size="small" type="danger">禁用</el-tag>
+          <el-tag v-else size="small">正常</el-tag>
+        </template>
+      </el-table-column>
+      <!--      <el-table-column-->
+      <!--        prop="roleIdList"-->
+      <!--        header-align="center"-->
+      <!--        align="center"-->
+      <!--        label="角色">-->
+      <!--      </el-table-column>-->
+      <!--      <el-table-column-->
+      <!--        prop="createTime"-->
+      <!--        header-align="center"-->
+      <!--        align="center"-->
+      <!--        width="180"-->
+      <!--        label="创建时间">-->
+      <!--      </el-table-column>-->
+      <el-table-column
+        fixed="right"
+        header-align="center"
+        align="center"
+        width="150"
+        label="操作">
+        <template slot-scope="scope">
+          <el-button v-if="isAuth('sys:user:save')" type="primary" @click="applyHandle(scope.row.doctorId)">申请</el-button>
+          <!--<el-button v-if="isAuth('sys:user:update')" type="text" size="small" @click="addOrUpdateHandle(scope.row.userId)">修改</el-button>-->
+          <!--<el-button v-if="isAuth('sys:user:delete')" type="text" size="small" @click="deleteHandle(scope.row.userId)">删除</el-button>-->
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <el-pagination
+      @size-change="sizeChangeHandle"
+      @current-change="currentChangeHandle"
+      :current-page="pageIndex"
+      :page-sizes="[10, 20, 50, 100]"
+      :page-size="pageSize"
+      :total="totalPage"
+      layout="total, sizes, prev, pager, next, jumper">
+    </el-pagination>
+    <!-- 弹窗, 新增 / 修改 -->
+    <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
+    <apply v-if="applyVisible" ref="apply" @refreshDataList="getDataList"></apply>
+  </div>
+</template>
+
+<script>
+  import AddOrUpdate from './doctor-add-or-update'
+  import Apply from './doctor-apply'
+  export default {
+    data () {
+      return {
+        dataForm: {
+          userName: ''
+        },
+        dataList: [],
+        pageIndex: 1,
+        pageSize: 10,
+        totalPage: 0,
+        dataListLoading: false,
+        dataListSelections: [],
+        addOrUpdateVisible: false,
+        applyVisible: false
+      }
+    },
+    components: {
+      Apply,
+      AddOrUpdate
+    },
+    activated () {
+      this.getDataList()
+    },
+    methods: {
+      // 获取数据列表
+      getDataList () {
+        this.dataListLoading = true
+        this.$http({
+          url: this.$http.adornUrl('/psychology/doctor/list'),
+          method: 'get',
+          params: this.$http.adornParams({
+            'page': this.pageIndex,
+            'limit': this.pageSize
+          })
+        }).then(({data}) => {
+          if (data && data.code === 0) {
+            console.log(data)
+            this.dataList = data.page.list
+            this.totalPage = data.page.totalCount
+          } else {
+            this.dataList = []
+            this.totalPage = 0
+          }
+          this.dataListLoading = false
+        })
+      },
+      // 当前页
+      currentChangeHandle (val) {
+        this.pageIndex = val
+        this.getDataList()
+      },
+      // 每页数
+      sizeChangeHandle (val) {
+        this.pageSize = val
+        this.pageIndex = 1
+        this.getDataList()
+      },
+      // 多选
+      selectionChangeHandle (val) {
+        this.dataListSelections = val
+      },
+      // 新增 / 修改
+      addOrUpdateHandle (id) {
+        this.addOrUpdateVisible = true
+        this.$nextTick(() => {
+          this.$refs.addOrUpdate.init(id)
+        })
+      },
+      // 申请
+      applyHandle (doctorId) {
+        console.log('doctorId是', doctorId)
+        this.applyVisible = true
+        this.$nextTick(() => {
+          this.$refs.apply.init(doctorId)
+        })
+      },
+      // 删除
+      deleteHandle (id) {
+        var userIds = id ? [id] : this.dataListSelections.map(item => {
+          return item.userId
+        })
+        this.$confirm(`确定对[id=${userIds.join(',')}]进行[${id ? '删除' : '批量删除'}]操作?`, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$http({
+            url: this.$http.adornUrl('/sys/user/delete'),
+            method: 'post',
+            data: this.$http.adornData(userIds, false)
+          }).then(({data}) => {
+            if (data && data.code === 0) {
+              this.$message({
+                message: '操作成功',
+                type: 'success',
+                duration: 1500,
+                onClose: () => {
+                  this.getDataList()
+                }
+              })
+            } else {
+              this.$message.error(data.msg)
+            }
+          })
+        }).catch(() => {})
+      }
+    }
+  }
+</script>
